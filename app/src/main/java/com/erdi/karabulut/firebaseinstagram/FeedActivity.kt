0200a24b9996c16c64b141aaafd18c97c0bb2dc9
Erk-06 <erdi.karabulut@erkteknoloji.com>
2019-11-14 16:39:54 +0300

    Resim paylaşma İşlemi yapıldı

--- app/src/main/java/com/erdi/karabulut/firebaseinstagram/FeedActivity.kt
+++ app/src/main/java/com/erdi/karabulut/firebaseinstagram/FeedActivity.kt
@@ -1,12 +1,64 @@
 package com.erdi.karabulut.firebaseinstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_feed.*
 
 class FeedActivity : AppCompatActivity() {
+
+    private lateinit var  auth:FirebaseAuth
+    private lateinit var db: FirebaseStorage
+    var userEmailFromFB:ArrayList<String> = ArrayList()
+    var userCommentFromFB: ArrayList<String> = ArrayList()
+    var userImageFromFB: ArrayList<String> = ArrayList()
+
+    var adapter: FeedRecyclerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+        auth= FirebaseAuth.getInstance()
+        db= FirebaseStorage.getInstance()
+
+        getDataFromFirestore()
+        //recyclerview
+        val linerLayoutManager=LinearLayoutManager(applicationContext)
+         recyclerView.layoutManager=linerLayoutManager
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInfalter=menuInflater
+        menuInfalter.inflate(R.menu.options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId==R.id.add_post)
+        {
+            val  intent=Intent(applicationContext,UploadActivity::class.java)
+            startActivity(intent)
+
+        }
+        else if(item.itemId==R.id.logout)
+        {
+            auth.signOut()
+            val intent=Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    fun getDataFromFirestore() {
+
     }
 }
--- app/src/main/java/com/erdi/karabulut/firebaseinstagram/FeedRecyclerAdapter.kt
+++ app/src/main/java/com/erdi/karabulut/firebaseinstagram/FeedRecyclerAdapter.kt
@@ -1,4 +1,26 @@
 package com.erdi.karabulut.firebaseinstagram
 
-class FeedRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class FeedRecyclerAdapter(private  val userEmailArray: ArrayList<String>, private val userCommentArray: ArrayList<String>, private val userImageArray: ArrayList<String>) :RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.recycler_view_row,parent,false)
+        return PostHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return userEmailArray.size
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+    }
+
+    class PostHolder(view: View) :RecyclerView.ViewHolder(view){
+
+
+}
 }
