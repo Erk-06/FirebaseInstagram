1675db5cde2910f317848cdeb6bd82c3b1303c0b
Erk-06 <erdi.karabulut@erkteknoloji.com>
2019-11-15 10:34:37 +0300

    Instagram

--- app/src/main/java/com/erdi/karabulut/firebaseinstagram/FeedActivity.kt
+++ app/src/main/java/com/erdi/karabulut/firebaseinstagram/FeedActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_feed.*
 
 class FeedActivity : AppCompatActivity() {
@@ -31,6 +33,8 @@ class FeedActivity : AppCompatActivity() {
         //recyclerview
         val linerLayoutManager=LinearLayoutManager(applicationContext)
          recyclerView.layoutManager=linerLayoutManager
+        adapter=FeedRecyclerAdapter(userEmailFromFB,userCommentFromFB,userImageFromFB)
+        recyclerView.adapter=adapter
 
 
     }
@@ -60,7 +64,42 @@ class FeedActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
     fun getDataFromFirestore() {
-       
 
+db.collection("Posts").orderBy("date", Query.Direction.DESCENDING).addSnapshotListener { snapshot, exception ->
+
+    if (exception != null) {
+        Toast.makeText(applicationContext,exception.localizedMessage.toString(), Toast.LENGTH_LONG).show()
+    } else {
+        if (snapshot!=null&&!snapshot.isEmpty)
+        {
+              userEmailFromFB.clear()
+            userCommentFromFB.clear()
+            userImageFromFB.clear()
+
+            val documents=snapshot.documents
+
+            for (document in documents)
+            {   val comment=document.get("comment") as String
+                val userEmail=document.get("userEmail") as String
+                val downloadUrl=document.get("downloadUrl") as String
+                val timestamp=document.get("date") as Timestamp
+                val date=timestamp.toDate()
+
+                userEmailFromFB.add(userEmail)
+                userCommentFromFB.add(comment)
+                userImageFromFB.add(downloadUrl)
+
+                adapter?.notifyDataSetChanged()
+
+
+
+
+            }
+
+
+        }
+
+    }
+}
     }
 }
